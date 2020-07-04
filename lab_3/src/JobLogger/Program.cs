bd5d770cc6c08ba110662355a17f1871f4c1528a
shipa <prospeedcuber@gmail.com>
2020-07-04 18:42:19 +0300

    add lab_3

---
+++ lab_3/src/JobLogger/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NATS.Client;
+using Subscriber;
+
+namespace JobLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var loggerservice = new LoggerService();
+
+            try {
+            using(IConnection connection = new ConnectionFactory().CreateConnection(ConnectionFactory.GetDefaultOptions())){
+                loggerservice.Run(connection);
+                Console.WriteLine("Logger tasks");
+                Console.ReadKey();
+            }
+            } catch(NATS.Client.NATSNoServersException e) {
+                Console.WriteLine("Not connected to NATS server");
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
